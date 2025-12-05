d14cb56415095e984d33c967916cbc76c24938db
Avalfortz <dabrowksi.stanislaw3141@gmail.com>
2025-12-05 22:35:20 +0100

    changed constructor logic

--- chess engine C++/SearchResult.cpp
+++ chess engine C++/SearchResult.cpp
@@ -3,4 +3,4 @@
 SearchResult::SearchResult(int16_t score, Move best_move)
 	: score(score), best_move(best_move) { }
 SearchResult::SearchResult(int16_t score)
-	: score(score), best_move() { }
+	: score(score) { }
