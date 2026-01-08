1dbf92f005bef47bd88b791c5dbef3335de0dbcd
Avalfortz <dabrowksi.stanislaw3141@gmail.com>
2026-01-08 17:05:53 +0100

    added beta and alpha scores types for quiescance

--- chess engine C++/ScoreType.h
+++ chess engine C++/ScoreType.h
@@ -3,8 +3,10 @@
 
 enum ScoreType : uint8_t
 {
-	EXACT,
-	ALPHA,//lower bound
-	BETA,//upper bound
-	QUIESCANCE
+	EXACT = 0,
+	ALPHA = 1,
+	BETA = 2,
+	QUIESCANCE_EXACT = 3,
+	QUIESCANCE_ALPHA = 4,
+	QUIESCANCE_BETA = 5
 };;
