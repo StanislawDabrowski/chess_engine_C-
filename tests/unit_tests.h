cb9366cf0a8d3dd87f1ea5b43ddee153f274c4fb
Avalfortz <dabrowksi.stanislaw3141@gmail.com>
2026-01-17 17:12:17 +0100

    added .h file for unit tests after the last change

--- tests/unit_tests.h
+++ tests/unit_tests.h
@@ -4,4 +4,4 @@
 
 int run_unit_tests(const int NUM_OF_POSITIONS_TO_TEST);
 
-int run_unit_tests_with_perft(const int NUM_OF_POSITIONS_TO_TEST, const uint8_t perft_dewpth);
+int run_unit_tests_with_perft(const int NUM_OF_POSITIONS_TO_TEST, const uint8_t perft_dewpth, const bool reset_counter);
